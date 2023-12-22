ad533fc808c669f93e2f5dd98a6d9f9604fd69cd
Julian Sanchez <alejo15san95@gmail.com>
2023-12-22 17:25:23 -0500

    fix: correction of the body modification
    A custom type was created and configured to recognize the uid field of the body that has the  request. This with the objective of being able to add this field to the body and to be able to access it in the different controllers that are handled and also so that typescript does not complain.

--- src/middlewares/validateJWT.ts
+++ src/middlewares/validateJWT.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Response, Request } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { CustomRequest } from "../interfaces";
 
 export const validteJWT = async (
   req: Request,
@@ -19,7 +18,7 @@ export const validteJWT = async (
   try {
     const secretKey = process.env.JWT_SECRET ?? "";
     const { uid } = jwt.verify(token, secretKey) as JwtPayload;
-    (req as CustomRequest).uid = uid;
+    req.uid = uid;
     next();
   } catch (error) {
     res.status(401).json({ ok: false, message: "Token invalid" });
